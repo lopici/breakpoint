92452d5cd3ab933d3ac799a3e773ffbff0d735b1
Andras <andras@MacBook.local>
2017-12-07 21:09:44 +1100

    l148

---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by Andras on 7/12/17.
+//  Copyright © 2017 Andras. All rights reserved.
+//
+
+import UIKit
+//SEND button will slide up with keyboard
+//call it in the viewcontroller on the button we want to slide (or anythung which inherits from UIView this can be used on
+extension UIView {
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endFrame.origin.y - beginningFrame.origin.y //monitor the change on the y axis
+        
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+    }
+}
