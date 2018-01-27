eeaa845bc8293e2ba6bb043cac4ab96ea9a2b59e
Andras <andras@MacBook.local>
2018-01-27 23:29:49 +1100

    l159

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -18,11 +18,25 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var sendBtnView: UIButton!
     @IBOutlet weak var sendBtn: UIButton!
     @IBOutlet weak var messageTextField: InsetTextField!
+    var group: Group?
+    
+    func initData(forGroup group: Group) {
+        self.group = group
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         sendBtnView.bindToKeyboard()
         
        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
+        membersLbl.text = group?.members.joined(separator: ", ")
+        
     }
 
     @IBAction func backBtnWasPressed(_ sender: Any) {
